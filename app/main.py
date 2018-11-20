957ba44a78f2c312288650ddb631569434ebda1c
ChelseaRDinh <chelseadinh94@gmail.com>
2018-11-19 19:03:36 -0800

    Created default board to print out.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 import bottle
 import os
 import random
-
-
+import numpy as np
 
 @bottle.route('/')
 def static():
@@ -17,24 +16,29 @@ def static(path):
 @bottle.post('/start')
 def start():
     # TODO: Do things with data
-	return {'Orange': 0xffa500}
+	#return {'Orange': 0xffa500}
+    data = bottle.request.json
+    board_width = data.get('width')
+    board_height = data.get('height')
+    #create empty game board.
+    game_board = np.chararray((board_height, board_width))
+    game_board[:] = '-'
+    print game_board
 
 @bottle.post('/move')
 def move():
     data = bottle.request.json
-
     # TODO: Do things with data
-    
     directions = ['up', 'down', 'left', 'right']
     direction = 'right'
-    print direction
+    #print direction
+    #return next move
     return {'move': direction}
 
 # Expose WSGI app (so gunicorn can find it)
 
-@bottle.post('/move')
-def move():
-	return {}
+def board_output(board_width, board_height):
+    return 0
 
 application = bottle.default_app()
 
